ad5e029d07f1cbbf2763de895a5c1daf9edb14a8
Art Rosnovsky <art@rosnovsky.us>
2024-02-04 16:32:33 -0800

    chore: add try/catch

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
 import fs from "node:fs";
+import path from "node:path";
 import { mdToPdf } from "md-to-pdf";
 
-(async () => {
-  const pdf = await mdToPdf({ path: "./resume.md" }).catch(console.error);
+const markdownFilePath = "./resume.md";
+const outputFilePath = "output/art_rosnovsky_software_engineer.pdf";
 
-  if (pdf) {
-    fs.writeFileSync("output/art_rosnovsky_software_engineer.pdf", pdf.content);
+(async () => {
+  if (!fs.existsSync(markdownFilePath)) {
+    console.error(`File ${markdownFilePath} does not exist.`);
+    throw new Error(`File ${markdownFilePath} does not exist.`);
+  }
+
+  try {
+    const pdf = await mdToPdf({ path: markdownFilePath });
+
+    if (pdf) {
+      fs.writeFileSync(path.resolve(__dirname, outputFilePath), pdf.content);
+      console.log(`PDF has been written to ${outputFilePath}`);
+    }
+  } catch (error: any) {
+    console.error(`Error occurred: ${error.message}`);
+    throw new Error(error.message);
   }
 })();
